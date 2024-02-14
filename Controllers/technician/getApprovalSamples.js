bb6314e3d074cb2344b3e94f42352d2745ac1b21
mohammad AL-haj <mo7ammad_7aaj@hotmail.com>
2024-02-15 00:55:48 +0300

    filters

--- Controllers/technician/getApprovalSamples.js
+++ Controllers/technician/getApprovalSamples.js
@@ -1,10 +1,37 @@
 const Sample = require("../../models/sample");
 const Study = require("../../models/study");
 const Patient = require("../../models/patient");
+const { Op } = require("sequelize");
 const getApprovalSamples = async (req, res) => {
   try {
+    const { searchData } = req.query;
+    const filter = {};
+    if (searchData) {
+      filter[Op.or] = [
+        {
+          sampleSerial: {
+            [Op.like]: `%${searchData}%`,
+          },
+        },
+        {
+          khccBioSampleCode: {
+            [Op.like]: `%${searchData}%`,
+          },
+        },
+        {
+          "$Patient.mrn$": {
+            [Op.like]: `%${searchData}%`,
+          },
+        },
+        {
+          "$Patient.ssn$": {
+            [Op.like]: `%${searchData}%`,
+          },
+        },
+      ];
+    }
     const samples = await Sample.findAll({
-      where: { isApproved: true, isStored: false },
+      where: { isApproved: true, isStored: false, ...filter },
       include: [
         {
           model: Study,
--- Controllers/technician/getSamples.js
+++ Controllers/technician/getSamples.js
@@ -1,9 +1,47 @@
 const Sample = require("../../models/sample");
 const Study = require("../../models/study");
 const Patient = require("../../models/patient");
+const { Op } = require("sequelize");
 
 const getSamples = async (req, res) => {
   try {
+    const { searchData, isApproved, isRejected } = req.query;
+    const filter = {};
+    if (searchData) {
+      filter[Op.or] = [
+        {
+          sampleSerial: {
+            [Op.like]: `%${searchData}%`,
+          },
+        },
+        {
+          "$Study.studyNumber$": {
+            [Op.like]: `%${searchData}%`,
+          },
+        },
+        {
+          "$Patient.patientName$": {
+            [Op.like]: `%${searchData}%`,
+          },
+        },
+        {
+          "$Patient.mrn$": {
+            [Op.like]: `%${searchData}%`,
+          },
+        },
+        {
+          "$Patient.ssn$": {
+            [Op.like]: `%${searchData}%`,
+          },
+        },
+      ];
+    }
+    if (isRejected !== undefined) {
+      filter.isRejected = isRejected === "true";
+    }
+    if (isApproved !== undefined) {
+      filter.isApproved = isApproved === "true";
+    }
     const samples = await Sample.findAll({
       include: [
         {
@@ -15,6 +53,7 @@ const getSamples = async (req, res) => {
           required: true,
         },
       ],
+      where: filter,
     });
 
     res.status(200).json({
